5ad51bdf79193ebc737c3e2ea341d7bf9e7baa4d
lubinpc <lubinpc@gmail.com>
2020-10-30 11:59:38 -0600

    first commit

---
+++ Sucursales/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sucursales.Startup))]
+namespace Sucursales
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
